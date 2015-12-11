a0da7f0361b5eea0a694579a9d05781d53ccadd2
hector <hectorluis.collado@gmail.com>
2015-12-11 14:38:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+module.exports = {
+    jonOk: function(){
+        return function(req, res, next){
+            res.jsonOk = function(data) {
+                var result = {
+                    ok: true
+                }
+
+                if (data)
+                    for(let key in data)
+                        result[key] = data[key];
+
+                res.json(result)
+            }
+
+            next()
+        }
+    },
+    jsonError: function(){
+        return function(req, res, next){
+            res.jsonError = function(error, data) {
+                var result = {
+                    ok: false,
+                    error: error
+                }
+
+                if (data)
+                    for(let key in data)
+                        result[key] = data[key];
+
+                res.json(result)
+            }
+
+            next()
+        }
+    }
+}
